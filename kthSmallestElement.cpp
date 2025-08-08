c13776bfb391fe4a5d86e0b024d85d056b6c89e3
Md Golam Bin As-Sami <144956960+Samii10@users.noreply.github.com>
2025-08-08 16:43:43 +0600

    Add files via upload
    Question: Find the kth smallest element from an array. 
    
    Input                                 Output
    a = [6,5,1,3,4],  k = 2             3
    a = [7,9,1,8,2],  k = 4             8

---
+++ kthSmallestElement.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+using namespace std;
+
+void Sort(int arr[], int n){
+    for(int i=0; i<n-1; i++){
+        for(int j=0; j<n-1-i; j++){
+            if(arr[j]>arr[j+1]){
+                int temp=arr[j];
+                arr[j]=arr[j+1];
+                arr[j+1]=temp;
+            }
+        }
+    }
+
+}
+int main(){
+
+    int n;
+    cout<<"Enter the number of elements: ";
+    cin>>n;
+
+    int arr[n];
+    cout<<"Enter "<<n<<" elements: ";
+    for(int i=0; i<n; i++){
+        cin>>arr[i];
+    }
+    int k;
+    cout<<"Enter the Kth value: ";
+    cin>>k;
+
+    Sort(arr, n);
+    if(k>=1 && k<=n){
+        cout <<"Smallest Kth value is: "<<arr[k-1];
+    }
+    else
+        cout<<"Error! Not Found.:;
+
+    return 0;
+
+
+}
