df46c68670fc09785ca2aa0c2cc64374f2f62da8
Md Golam Bin As-Sami <144956960+Samii10@users.noreply.github.com>
2025-08-10 01:37:18 +0600

    Add files via upload

---
+++ consequenses.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+
+using namespace std;
+void Sort(int arr[], int n){
+    for(int i=0; i<n-1; i++){
+        for(int j=0; j<n-1-i; j++){
+            if(abs(arr[j])>abs(arr[j+1])){
+                int temp=arr[j];
+                arr[j]=arr[j+1];
+                arr[j+1]=temp;
+            }
+        }
+    }
+}
+int main(){
+
+    int n, i;
+
+    cout<<"enter elements number: "<<endl;
+    cin>>n;
+    int arr[n];
+
+    cout<<"Enter the elements of the array: ";
+    for(i=0; i<n; i++){
+        cin>>arr[i];
+    }
+
+Sort(arr, n);
+
+    int cnt=1;
+    int result=1;
+
+    for(int i=1; i<n; i++){
+        if(arr[i]==arr[i-1])
+            continue;
+
+    if(arr[i]==arr[i-1]+1){
+        cnt++;
+    }
+    else{
+        cnt=1;
+    }
+
+result=max(result, cnt);
+}
+
+cout<<result;
+
+}
+
+
+
+
