288468c0751afd0ac38a0ec2641cd88ce1d6ce6a
Md Golam Bin As-Sami <144956960+Samii10@users.noreply.github.com>
2025-10-19 02:06:09 +0600

    Add files via upload

---
+++ Valied Parenthases.cpp
@@ -0,0 +1,65 @@
+#include<iostream>
+using namespace std;
+
+class Stack{
+    char arr[100];
+    int top;
+public:
+    Stack(){
+        top=-1;
+    }
+
+    void push(char c){
+        arr[++top]=c;
+    }
+
+    char pop(){
+        if(top==-1){
+            return '\0';
+        }
+        return arr[top--];
+    }
+
+    bool isEmpty(){
+        return top==-1;
+    }
+};
+
+bool isValied(string s){
+    Stack st;
+
+    for(char ch : s){
+        if(ch=='(' || ch=='{' || ch=='[' ){
+            st.push(ch);
+           }
+           else
+           {
+               char topChar=st.pop();
+
+           if((ch==')' && topChar !='(') ||
+              (ch=='}' && topChar !='{') ||
+              (ch==']' && topChar !='[')){
+                return false;
+              }
+              }
+    }
+    return st.isEmpty();
+}
+
+int main()
+{
+    string s;
+    cout<<"Enter strings with brackets: "<<endl;
+    cin>>s;
+
+    if(isValied(s))
+    {
+        cout<<"Valid parentheses"<<endl;
+    }
+    else
+    {
+        cout<<"Invalid parentheses"<<endl;
+    }
+    return 0;
+
+}
