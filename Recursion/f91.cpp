c496e00b69d61a8fe1cc76492da4db7b6f7bed3e
Md Golam Bin As-Sami <144956960+Samii10@users.noreply.github.com>
2026-01-03 18:22:29 +0600

    Add files via upload

---
+++ Recursion/f91.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int f91(int N)
+{
+    if(N>=101)
+    return  N-10;
+    
+    return f91(f91(N+11));
+
+}
+int main()
+{
+    int N;
+    while (cin>>N && N!=0)
+    {
+      cout<<"f91("<<N<<") = "<<f91(N)<<endl; 
+    }
+    return 0;
+}
